b899738c644b27e4c15129ae858ab098f1d936e9
Derrick Reimer <derrickreimer@gmail.com>
2023-07-08 18:57:09 -0500

    Install dialyzer

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule Inertia.MixProject do
       {:plug_cowboy, "~> 2.1", only: :test},
       {:floki, ">= 0.30.0", only: :test},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.27", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
 
