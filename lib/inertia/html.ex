d6a8c835a50440e8b4ed1976dcf41f5745f82732
Derrick Reimer <derrickreimer@gmail.com>
2025-02-28 08:15:06 -0600

    Add some docs on the inertia_title tag

--- lib/inertia/html.ex
+++ lib/inertia/html.ex
@@ -11,6 +11,31 @@ defmodule Inertia.HTML do
     doc: "A prefix added before the content of `inner_block`."
   )
 
+  @doc """
+  Renders a `<title>` tag that includes the `inertia` attribute needed for the
+  Inertia.js client-side to subsequently manage the page title.
+
+  The content you provide to this component will only apply to the initial
+  server-rendered response. You will also need to use the [client-side `<Head>`
+  component](https://inertiajs.com/title-and-meta) in your Inertia page
+  components to be sure the the page title is updated when internal navigation
+  occurs.
+
+  If you are planning to manage page titles from the server-side, you may find
+  it useful to expose the `page_title` via regular assigns (so your
+  `<.inertia_title>` can use it) AND via Inertia page props (so your client-side
+  `<Head>` use it):
+
+      def index(conn, _params)
+        page_title = "Your page title"
+
+        conn
+        |> assign(:page_title, page_title)
+        |> assign_prop(:page_title, page_title)
+        |> render_inertia("YourPage")
+      end
+
+  """
   attr(:suffix, :string, default: nil, doc: "A suffix added after the content of `inner_block`.")
   slot(:inner_block, required: true, doc: "Content rendered inside the `title` tag.")
 
