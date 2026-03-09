a42d23a380320f84b31c1a35b112228e5065ce8c
oscar345 <o.zwagers@icloud.com>
2026-03-09 11:31:51 +0100

    Also remove axios from the igniter install script

--- lib/mix/tasks/inertia.install.ex
+++ lib/mix/tasks/inertia.install.ex
@@ -349,12 +349,9 @@ if Code.ensure_loaded?(Igniter) do
     defp inertia_app_jsx() do
       """
       import React from "react";
-      import axios from "axios";
 
       import { createInertiaApp } from "@inertiajs/react";
       import { createRoot } from "react-dom/client";
-
-      axios.defaults.xsrfHeaderName = "x-csrf-token";
 
       createInertiaApp({
         resolve: async (name) => {
@@ -362,6 +359,9 @@ if Code.ensure_loaded?(Igniter) do
         },
         setup({ App, el, props }) {
           createRoot(el).render(<App {...props} />);
+        },
+        http: {
+          xsrfHeaderName: "x-csrf-token",
         },
       });
       """
--- test/mix/tasks/inertia.install_test.exs
+++ test/mix/tasks/inertia.install_test.exs
@@ -176,12 +176,9 @@ defmodule Mix.Tasks.Inertia.InstallTest do
       # Assert the app.jsx file is created
       assert_creates(project, "assets/js/app.jsx", """
       import React from "react";
-      import axios from "axios";
 
       import { createInertiaApp } from "@inertiajs/react";
       import { createRoot } from "react-dom/client";
-
-      axios.defaults.xsrfHeaderName = "x-csrf-token";
 
       createInertiaApp({
         resolve: async (name) => {
@@ -189,6 +186,9 @@ defmodule Mix.Tasks.Inertia.InstallTest do
         },
         setup({ App, el, props }) {
           createRoot(el).render(<App {...props} />);
+        },
+        http: {
+          xsrfHeaderName: "x-csrf-token",
         },
       });
       """)
