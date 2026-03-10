fdb3e501eefed21f6849c5e39eda8c98039fb363
Derrick Reimer <derrickreimer@gmail.com>
2026-03-10 15:49:18 -0500

    Elixir 1.18+ compatibility: upgrade deps, fix credo, fix formatter
    - Upgrade igniter ~> 0.5 to ~> 0.7 (drops inflex, incompatible with 1.18)
    - Upgrade credo to 1.7.17 (fixes regex compilation on 1.19)
    - Fix credo warnings: replace length/1 guards with pattern matching,
      remove parens from zero-arity functions, alphabetize aliases, add
      module aliases to reduce nested references in installer
    - Exclude test fixture from formatter (Elixir 1.18 ~H sigil change)
    - Update CI matrix to Elixir 1.18/1.19
    - Update esbuild test expectations for phx_new 1.8.5 fixture

--- test/inertia/errors_test.exs
+++ test/inertia/errors_test.exs
@@ -1,8 +1,8 @@
 defmodule Inertia.ErrorsTest do
   use ExUnit.Case, async: true
 
-  alias Inertia.Errors
   alias Ecto.Changeset
+  alias Inertia.Errors
 
   describe "to_errors/1 for Map" do
     test "passes through valid errors" do
--- test/mix/tasks/inertia.install_test.exs
+++ test/mix/tasks/inertia.install_test.exs
@@ -119,14 +119,15 @@ defmodule Mix.Tasks.Inertia.InstallTest do
       ...|
          |# Configure esbuild (the version is required)
          |config :esbuild,
-       - |  version: "0.17.11",
+       - |  version: "0.25.4",
        + |  version: "0.21.5",
          |  test: [
          |    args:
-       - |      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+       - |      ~w(js/app.js --bundle --target=es2022 --outdir=../priv/static/assets/js --external:/fonts/* --external:/images/* --alias:@=.),
        + |      ~w(js/app.jsx --bundle --chunk-names=chunks/[name]-[hash] --splitting --format=esm  --target=es2020 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
          |    cd: Path.expand("../assets", __DIR__),
-         |    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+       - |    env: %{"NODE_PATH" => [Path.expand("../deps", __DIR__), Mix.Project.build_path()]}
+       + |    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
       ...|
       """)
 
